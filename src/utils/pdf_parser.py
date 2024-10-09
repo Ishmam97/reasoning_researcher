854154baaed9e63ceffd3f4264958b295f4cd24d
Aatif Muhammad <aatif@Aatifs-MacBook-Pro.local>
2024-10-08 22:54:27 -0500

    Modifed the parsers

--- src/utils/pdf_parser.py
+++ src/utils/pdf_parser.py
@@ -1,9 +1,11 @@
 from PyPDF2 import PdfReader
 
 class PDFReader:
-    def pdf_extract_content(self,pdf_file):
 
-        self.text=""
+    @staticmethod
+    def pdf_extract_content(pdf_file):
+
+        text=""
 
         for pdf in pdf_file:
             try:
@@ -15,10 +17,10 @@ class PDFReader:
                 for page in pdf_reader.pages:
                     page_text = page.extract_text()
                     if page_text:
-                        self.text += page_text
+                        text += page_text
                     else:
                         raise Exception(f"Failed to extract text from a page in {pdf}.")
             except Exception as e:
                 raise Exception(f"Error occurred while extracting text from {pdf}. Error: {e}")
             
-        return self.text
+        return text
--- src/utils/word_parser.py
+++ src/utils/word_parser.py
@@ -1,14 +1,14 @@
 import docx2txt 
 
 class DocumentReader:
-    def doc_extract_content(self,doc_file):
+    def doc_extract_content(doc_file):
         
-        self.text = ""
+        text = ""
 
         for doc in doc_file:
             try:
-                self.text += docx2txt.process(doc)
+                text += docx2txt.process(doc)
             except Exception as e:
                 raise Exception(f"Error occured while extracting text from {doc}. Error: {e} ")
      
-        return self.text
+        return text
